1775aed21ee78a14c57518ee1a27eee0ba2b7385
s4timuen <tim.muenzinger@yahoo.de>
2023-03-06 22:18:27 +0100

    added check for correct jwt in cookies, on app start

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
 
 <script lang='js'>
 import store from '@/store/store';
+import router from '@/router/router';
 
 import Header from '@/views/components/header';
 import Footer from '@/views/components/footer';
@@ -18,11 +19,23 @@ export default {
     Footer,
   },
   mounted: function () {
-
     const cookies = document.cookie.split(';')
-    cookies.forEach(cookie => {
+
+    cookies.forEach(async cookie => {
       if(cookie.startsWith('jwt=')) {
-        store.commit('setJwt', cookie.replace('jwt=', ''));
+
+        const jwt = cookie.replace('jwt=', '');
+        store.getters.getApiWrapper.setJwt(jwt);
+        const res = await store.getters.getApiWrapper.getMe();
+
+        if (res.status === 'error') {
+          store.commit('setJwt', '');
+          router.push({ path: '/login' });
+        }
+        if(res.status === 'success') {
+          store.commit('setJwt', jwt);
+          store.commit('setUser', res.data.data);
+        }
       }
     });
   }
